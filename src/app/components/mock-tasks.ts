05ec1c7b682382be445c5fbfa9d4dda6caf365c2
Gonzalo Pereira <pereiradossantosgonzalo@gmail.com>
2022-08-10 16:00:15 -0300

    agregado mock tasks

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,22 @@
+export const TASKS = [
+
+    {
+        id: 1,
+        text: "Terminar primer modulo",
+        day: "Agosto 5 a las 12:00",
+        reminder: true 
+    },
+    {
+        id: 2,
+        text: "Terminar segundo modulo",
+        day: "Agosto 10 a las 17:00",
+        reminder: true 
+    },
+    {
+        id: 3,
+        text: "Comprar leche",
+        day: "Agosto 6 a las 17:00",
+        reminder: true 
+    },
+
+]
